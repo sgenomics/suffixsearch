72c2f02cb88e20c7f30627e5203ca0c55282d94d
new <new@dd0af01d-149e-4901-af2f-bc70ade6c2fb>
2011-01-30 17:45:46 +0000

    tidying up
    git-svn-id: svn+ssh://sgenomics.org/home/new/svn-repos/code/suffix4@307 dd0af01d-149e-4901-af2f-bc70ade6c2fb

--- ChildStore.h
+++ ChildStore.h
@@ -25,36 +25,23 @@ public:
 
   void set(char symbol,int index) {
 
-//    cout << "adding child: " << static_cast<int>(symbol) << "," << index << endl;
     if(get(symbol) == -1) {
       m_symbols.push_back(SymbolPair(symbol,index));
     } else
     for(size_t n=0;n<m_symbols.size();n++) {
       if(m_symbols[n].symbol == symbol) {
-   ////     if(index != -1) m_symbols[n].index = index;
         m_symbols[n].index = index;
         if(index == -1) {m_symbols.erase(m_symbols.begin()+n,m_symbols.begin()+n); break;}
       }
     }
 
-    // dump
- /*   for(size_t n=0;n<m_symbols.size();n++) {
-      cout << "child list: " << static_cast<int>(m_symbols[n].symbol) << "," << m_symbols[n].index << endl;
-    }
-*/
   }
 
   int get(char symbol) {
-//    cout << "ChildStore, getting child: " << static_cast<int>(symbol) << endl;
-//    cout << "m_symbols.size(): " << m_symbols.size() << endl;
     for(size_t n=0;n<m_symbols.size();n++) {
       if(m_symbols[n].symbol == symbol) {
-//        cout << "found at position: " << n << endl;
-//        cout << "returning: " << m_symbols[n].index << endl;
         return m_symbols[n].index;
       }
-
-//      cout << "not found at position: " << n << " " << static_cast<int>(m_symbols[n].symbol)  << "," << m_symbols[n].index << endl;
     }
     return -1;
   }
