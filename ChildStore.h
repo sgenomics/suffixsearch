cf8f0209c624c080f53b2fbc64083b978965df43
new <new@dd0af01d-149e-4901-af2f-bc70ade6c2fb>
2011-01-30 17:56:46 +0000

    bug fix
    git-svn-id: svn+ssh://sgenomics.org/home/new/svn-repos/code/suffix4@309 dd0af01d-149e-4901-af2f-bc70ade6c2fb

--- ChildStore.h
+++ ChildStore.h
@@ -66,11 +66,11 @@ public:
     return true;
   }
 
-  vector<SymbolPair> get_symbols() {
+  vector<SymbolPair> &get_symbols() {
     return m_symbols;
   }
 
-  void set_symbols(vector<SymbolPair> &s) {
+  void set_symbols(vector<SymbolPair> s) {
     m_symbols = s;
   }
 
