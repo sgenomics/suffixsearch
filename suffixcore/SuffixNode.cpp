5285d016da802b0a38a47760795976114637416a
Nava Whiteford <new@sgenomics.org>
2012-04-17 07:16:28 +0100

    removed files which now live in their own repos

--- suffixcore/SuffixNode.cpp
+++
@@ -1,5 +0,0 @@
-#include "SuffixNode.h"
-
-int SuffixNode::end_marker = -1;
-int SuffixNode::end_marker_value = -1;
-int SuffixNode::root = 0;
--- suffixcore_optional/SuffixNodeStore.cpp
+++
@@ -1,5 +0,0 @@
-#include "SuffixNodeStore.h"
-#include "ObjectStoreMem.h"
-#include <stdint.h>
-
-int32_t NormalSuffixNodeContainer::invalidation_count = 0;
