a54eac916bdead93f2f12f6c870e6dffc26644c7
Nava Whiteford <new@sgenomics.org>
2012-04-17 05:15:28 +0100

    notes in global_defs.h

--- global_defs.h
+++ global_defs.h
@@ -4,10 +4,12 @@
 #define suffixnodestore_type SuffixNodeStoreMemVec
 //#define suffixnodestore_type SuffixNodeStore
 
+//define one of these to delect a storage mode
 //#define object_store_type ObjectStoreMem
 #define object_store_type ObjectStoreMemVec
 //#define object_store_type ObjectStoreDisk
 
+//define one of these to select a memory allocator
 //#define use_malloc 1
 #define use_tialloc 1
 
